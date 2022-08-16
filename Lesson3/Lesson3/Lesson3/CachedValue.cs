849049fbfa56ad34366fdd09d02f8e2c03189987
Alex <alexshaposhnikoff@mail.ru>
2022-08-16 17:36:58 +0300

    HomeWork 3 Done

---
+++ Lesson3/Lesson3/Lesson3/CachedValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    internal record CachedValue<T>
+    {
+        public T? Value { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public int Timeout { get; set; }
+    }
+}
--- Lesson3/Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Lesson3/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        private static SimpleGenericCache<string> _stringCache;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _stringCache = new SimpleGenericCache<string>();
+
+            TestCache();
+
+            Console.ReadKey();
+        }
+
+        private static void TestCache()
+        {
+            _stringCache.Store("FIRST", "OK");
+            _stringCache.Store("SECOND", "NO_OK", 10);
+
+            Thread.Sleep(15000);
+
+            Console.WriteLine($"FIRST: {_stringCache.Fetch("FIRST")}");     // Should display a value
+            Console.WriteLine($"SECOND: {_stringCache.Fetch("SECOND")}");   // Should display a default
         }
     }
 }
